0dc8242be0891bbb8924793339262faac114a8c6
Soulbolt <soulbolt@gmail.com>
2021-02-24 10:41:27 -0700

    Added math values to substract and multiply functions

--- calculator.py
+++ calculator.py
@@ -1,13 +1,14 @@
 def add(x, y):
+
     result = #function here
     return result
 
 def subtract(x, y):
-    result = #function here
+    result = x - y
     return result
 
 def multiply(x, y):
-    result = #function here
+    result = x * y
     return result
 
 def divide(x, y):
