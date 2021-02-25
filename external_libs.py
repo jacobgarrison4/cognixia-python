3de8ec04456f89e7c01857ad84282b1ad0684f8b
jacobgarrison4 <jacobdgarrison4@gmail.com>
2021-02-25 14:28:33 -0600

    initial commit for libs

---
+++ external_libs.py
@@ -0,0 +1,29 @@
+
+#####################
+# Import space
+#####################
+import pandas as pd 
+import math
+
+######################
+# Functions/Constants/etc
+######################
+
+PI = math.pi
+E = math.e
+
+def func1():
+    pass
+def func2():
+    pass
+
+
+print("according to python internals, the name of the file is currently: ", __name__)
+####################
+# Main function space
+####################
+
+
+
+if __name__ == "__main__":
+    print("I am currently the main module!")
